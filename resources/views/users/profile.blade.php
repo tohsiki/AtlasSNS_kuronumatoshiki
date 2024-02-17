8c1c96076c6b18a528720c2370b21c6155c91edc
黒沼敏樹 <toshiki.kuronuma9@gmail.com>
2024-02-17 18:54:23 +0900

    ユーザープロフィールのcss変更。

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -16,54 +16,51 @@
 
 
 <div class="profile-container">
-    {!! Form::open(['url' => '/profile/update', 'enctype' => 'multipart/form-data']) !!}
-        {{ Form::hidden('id', $user->id) }}
-
-        <!-- ユーザー名 -->
-        <div class="profile-form">
-            {{ Form::label('ユーザー名') }}
-            {{ Form::input('text', 'upName', $user->username, [ 'class' => 'form-control']) }}
+    <div class="profile-form">
+            {!! Form::open(['url' => '/profile/update', 'enctype' => 'multipart/form-data']) !!}
+            {{ Form::hidden('id', $user->id) }}
+            <!-- ユーザー名 -->
+        <div class="profile-items">
+            <p>ユーザー名</p>
+            {{ Form::input('text', 'upName', $user->username, [ 'class' => 'profile-name up-form']) }}
+        </div>
+        <div class="profile-items">
+            <!-- アドレス -->
+            <p>メールアドレス</p>
+            {{ Form::input('text', 'upMail', $user->mail, [ 'class' => 'profile-mail up-form']) }}
+        </div>
+        <div class="profile-items">
+            <!-- パスワード -->
+            <p>パスワード</p>
+            {{ Form::password('newPass',[ 'class' => 'profile-pass up-form']) }}
+        </div>
+        <!-- パスワードは初期値入れない -->
+        <div class="profile-items">
+            <!-- パスワード確認用 -->
+            <p>パスワード確認</p>
+            {{ Form::password('newPassCon',  [ 'class' => 'profile-pass up-form']) }}
+        </div>
+        <div class="profile-items">
+            <!-- 自己紹介 -->
+            <p>自己紹介</p>
+            {{ Form::input('text', 'upBio', $user->bio, ['class' => 'profile-bio up-form']) }}
         </div>
 
-
-        <!-- アドレス -->
-        <div class="profile-form">
-            {{ Form::label('メールアドレス') }}
-            {{ Form::input('text', 'upMail', $user->mail, [ 'class' => 'form-control']) }}
+        <div class="profile-items update-icon">
+            <!-- アイコンの更新 -->
+            <!-- 選択されていませんを削除する。 -->
+            <p>アイコン更新</p>
+            <div class="icon-profile">
+                <label for="newIcon" class="custom-file-upload">
+                <input type="file" class="profile-icon icon-imag" name="newIcon"><p class="custom-file-upload">ファイルを選択</p>
+                </label>
+            </div>
+        </div>
+        <div class="">
+            <button type="submit" class="btn btn-danger pull-right update-btn">更新</button>
         </div>
 
-
-        <!-- 自己紹介 -->
-        <div class="profile-form">
-            {{ Form::label('自己紹介') }}
-             {{ Form::input('text', 'upBio', $user->bio, ['class' => 'form-control']) }}
-        </div>
-
-
-            <!-- パスワードは初期値入れない -->
-        <!-- パスワード -->
-        <div class="profile-form">
-            {{ Form::label('パスワード') }}
-            {{ Form::password('newPass', null,[ 'class' => 'form-control']) }}
-        </div>
-
-
-        <!-- パスワード確認用 -->
-        <div class="profile-form">
-            {{ Form::label('パスワード確認用') }}
-            {{ Form::password('newPassCon', null, [ 'class' => 'form-control']) }}
-        </div>
-
-         <div class="profile-form">
-            {{ Form::label('アイコン更新') }}
-            {{ Form::file('newIcon', ['class'=>'','id'=>'fileImage'] )}}
-        </div>
-
-
-        <button type="submit" class="btn btn-danger pull-right">更新</button>
-    {!! Form::close() !!}
+            {!! Form::close() !!}
+    </div>
 </div>
-
-
-
 @endsection
